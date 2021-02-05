3784df1fe412a422767b334193a6f5bee91048bd
17021084 <dotrunghg1999@gmail.com>
2021-02-05 10:17:56 +0700

    dont use redux. instead use context

---
+++ src/store/actions/actionType.js
@@ -0,0 +1,22 @@
+//* Auth actions
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+
+export const LOG_IN_ERROR = "LOG_IN_ERROR";
+
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+
+export const SIGN_UP_ERROR = "SIGN_UP_ERROR";
+
+export const CLEAR_ERROR_MESSAGE = "CLEAR_ERROR_MESSAGE";
+
+export const SIGN_OUT = "SIGN_OUT";
+
+export const LOADING_USER = "LOADING_USER";
+
+export const GET_INFO = "GET_INFO";
+
+export const POST_INFO = "POST_INFO";
+
+export const UPDATE_INFO_SUCCESS = "UPDATE_INFO_SUCCESS";
+
+export const UPDATE_INFO_FAILED = "UPDATE_INFO_FAILED";
